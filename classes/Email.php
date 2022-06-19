3472d1d95ed7624412db8d543d0861274f4b395f
reyes1010 <92321108+reyes1010@users.noreply.github.com>
2022-06-19 14:34:42 -0600

    PROYECTO FINALIZADO PROGRACION WEB II

---
+++ classes/Email.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Classes;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+class Email {
+
+    public $email;
+    public $nombre;
+    public $token;
+    
+    public function __construct($email, $nombre, $token)
+    {
+        $this->email = $email;
+        $this->nombre = $nombre;
+        $this->token = $token;
+    }
+
+    public function enviarConfirmacion() {
+
+         // create a new object
+         $mail = new PHPMailer();
+         $mail->isSMTP();
+         $mail->Host = 'smtp.mailtrap.io';
+         $mail->SMTPAuth = true;
+         $mail->Port = 2525;
+         $mail->Username = '9aa00d64385e6f';
+         $mail->Password = '0409a9ae36471c';
+     
+         $mail->setFrom('cuentas@abrahamstyle.com');
+         $mail->addAddress('cuentas@abrahamstyle.com', 'abrahamstyle.com');
+         $mail->Subject = 'Confirma tu Cuenta';
+
+         // Set HTML
+         $mail->isHTML(TRUE);
+         $mail->CharSet = 'UTF-8';
+
+         $contenido = '<html>';
+         $contenido .= "<p><strong>Hola " . $this->email .  "</strong> Has Creado tu cuenta en AbrahamStyle, solo debes confirmarla presionando el siguiente enlace</p>";
+         $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";        
+         $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
+         $contenido .= '</html>';
+         $mail->Body = $contenido;
+
+         //Enviar el mail
+         $mail->send();
+
+    }
+
+    public function enviarInstrucciones() {
+
+        // create a new object
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = '9aa00d64385e6f';
+        $mail->Password = '0409a9ae36471c';
+    
+        $mail->setFrom('cuentas@abrahamstyle.com');
+        $mail->addAddress('cuentas@abrahamstyle.com', 'abrahamstyle.com');
+        $mail->Subject = 'Confirma tu Cuenta';
+
+
+        // Set HTML
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
+
+        $contenido = '<html>';
+        $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/recuperar?token=" . $this->token . "'>Reestablecer Password</a>";        
+        $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
+        $contenido .= '</html>';
+        $mail->Body = $contenido;
+
+            //Enviar el mail
+        $mail->send();
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'elias/app-salon_php_mvc_js_sass_fin',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'elias/app-salon_php_mvc_js_sass_fin' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.0',
+            'version' => '6.6.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'e43bac82edc26ca04b36143a48bde1c051cfd5b1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
